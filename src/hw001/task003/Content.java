d236a16e630260e7ec33983a7f9539d529b8d3b6
samko.s <ssamko091184@gmail.com>
2022-05-15 00:13:36 +0300

    task003 done.

---
+++ src/hw001/task003/Content.java
@@ -0,0 +1,9 @@
+package hw001.task003;
+
+public class Content {
+    public String content;
+
+    public void show() {
+        System.out.printf("\nBrief about the book: %s.", content);
+    }
+}
---
+++ src/hw001/task003/Title.java
@@ -0,0 +1,9 @@
+package hw001.task003;
+
+public class Title {
+    public String title;
+
+    public void show() {
+        System.out.printf("\nTitle of the book is %s.", title);
+    }
+}
