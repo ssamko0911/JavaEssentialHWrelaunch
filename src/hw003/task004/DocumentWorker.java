4ed519de877c0226e8c1505959030ad9976f7574
samko.s <ssamko091184@gmail.com>
2022-05-18 16:58:18 +0300

    task003 done.

---
+++ src/hw003/task004/DocumentWorker.java
@@ -0,0 +1,15 @@
+package hw003.task004;
+
+public class DocumentWorker {
+    void openDocument() {
+        System.out.println("The document is opened.");
+    }
+
+    void editDocument() {
+        System.out.println("Editing the document is available in Pro version.");
+    }
+
+    void saveDocument() {
+        System.out.println("Saving the document is available in Pro version.");
+    }
+}
